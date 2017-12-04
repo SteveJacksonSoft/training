f987eb8aa8c144f73decb581412a99c973037b2a
Stephen Jackson <Stephen.Jackson@softwire.com>
2017-12-04 18:03:52 +0000

    The stretchy bits - still needs a bit more waterproofing for the options.

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,26 +1,43 @@
+const readline = require('readline-sync');
+
 let minJ,
     bIndex,
     readOut = "",
     oldOut = "",
-    isDiv = false;
+    rules = Array(6),
+    primes = ['3','5','7','11','13','17'];
 
-for (let i=1;i<=230;i++){
+// Input options
+console.log("How far do you want to go?");
+const topNum = + readline.prompt();
+
+console.log("Which rules would you like to implement? Enter a subset of {3,5,7,11,13,17}.");
+const options = readline.prompt();
+for( let i=0;i<6;i++){
+    if ( options.indexOf(' ' + primes[i] + ' ') !== -1 ){
+        rules[i] = true;
+    }else{
+        rules[i] = false;
+    }
+}
+
+for (let i=1;i<=topNum;i++){
     readOut = "";
 
     // For multiples of 3,5,7 read terms as and when and in that order
-    if (i%3 === 0)
-        readOut = "Fizz";
-    if (i%5 === 0)
+    if (i%3 === 0 && rules[0])
+        readOut += "Fizz";
+    if (i%5 === 0 && rules[1])
         readOut += "Buzz";
-    if (i%7 === 0)
+    if (i%7 === 0 && rules[2])
         readOut += "Bang";
 
     // For multiples of 11 read "Bong" only (unless also multiple of 13)
-    if (i%11 === 0)
+    if (i%11 === 0 && rules[3])
         readOut = "Bong";
 
     // For multiples of 13 read Fezz before first B word
-    if (i%13 ===0) {
+    if (i%13 ===0 && rules[4]) {
         bIndex = readOut.indexOf("B");
         if (bIndex !== -1)
             readOut = readOut.slice(0,bIndex)+ "Fezz" + readOut.slice(bIndex);
@@ -29,7 +46,7 @@ for (let i=1;i<=230;i++){
     }
 
     // For multiples of 17 reverse order of words
-    if (i%17===0){
+    if (i%17===0 && rules[5]){
         minJ = -readOut.length;
         oldOut = readOut;
         readOut = "";
