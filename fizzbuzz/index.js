37b19de955abac51f94eca842d4ae43c5ab39845
Stephen Jackson <Stephen.Jackson@softwire.com>
2017-12-04 17:18:44 +0000

    A small refinement

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -6,14 +6,20 @@ let minJ,
 
 for (let i=1;i<=230;i++){
     readOut = "";
+
+    // For multiples of 3,5,7 read terms as and when and in that order
     if (i%3 === 0)
         readOut = "Fizz";
     if (i%5 === 0)
         readOut += "Buzz";
     if (i%7 === 0)
         readOut += "Bang";
+
+    // For multiples of 11 read "Bong" only (unless also multiple of 13)
     if (i%11 === 0)
         readOut = "Bong";
+
+    // For multiples of 13 read Fezz before first B word
     if (i%13 ===0) {
         bIndex = readOut.indexOf("B");
         if (bIndex !== -1)
@@ -21,6 +27,8 @@ for (let i=1;i<=230;i++){
         else
             readOut += "Fezz";
     }
+
+    // For multiples of 17 reverse order of words
     if (i%17===0){
         minJ = -readOut.length;
         oldOut = readOut;
@@ -29,7 +37,10 @@ for (let i=1;i<=230;i++){
             readOut += oldOut.slice(j-4,j);
         }
     }
+
+    // If not multiple of any:
     if (readOut === "")
         readOut = i.toString();
+
     console.log(readOut);
 }
