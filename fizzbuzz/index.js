e1058635197406f96300f248b7e0a25fc160da0d
Stephen Jackson <Stephen.Jackson@softwire.com>
2017-12-05 10:55:10 +0000

    Refactored and hopefully working

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -7,11 +7,54 @@ let minJ,
     rules = Array(6),
     primes = ['3','5','7','11','13','17'];
 
+// Rules
+function rule3(num , readOut){
+    if ( num%3 === 0 )
+        readOut += "Fizz";
+    return readOut
+}
+function rule5(num , readOut){
+    if ( num%5 === 0 )
+        readOut += "Buzz";
+    return readOut
+}
+function rule7(num , readOut){
+    if ( num%7 === 0 )
+        readOut += "Bang";
+    return readOut
+}
+function rule11(num , readOut){
+    if ( num%11 === 0 )
+        readOut = "Bong";
+    return readOut
+}
+function rule13(num , readOut){
+    if ( num%13 === 0 ) {
+        bIndex = readOut.indexOf("B");
+        if (bIndex !== -1)
+            readOut = readOut.slice(0,bIndex)+ "Fezz" + readOut.slice(bIndex);
+        else
+            readOut += "Fezz";
+    }
+    return readOut
+}
+function rule17(num , readOut){
+    if ( num%17 === 0 ){
+        maxJ = readOut.length;
+        oldOut = readOut;
+        readOut = "";
+        for (let j=0;j<=maxJ;j=j+4){
+            readOut += oldOut.slice( maxJ-j , maxJ-j+4 );
+        }
+    }
+    return readOut
+}
+
 // Input options
 console.log("How far do you want to go?");
-const topNum = + readline.prompt();
+const topNum = readline.prompt();
 
-console.log("Which rules would you like to implement? Enter a subset of {3,5,7,11,13,17}.");
+console.log("Which rules would you like to implement? Enter a subset of { 3, 5, 7, 11, 13, 17}. Leave a space on each side of the number.");
 const options = readline.prompt();
 for( let i=0;i<6;i++){
     if ( options.indexOf(' ' + primes[i] + ' ') !== -1 ){
@@ -21,39 +64,29 @@ for( let i=0;i<6;i++){
     }
 }
 
+// Readout
 for (let i=1;i<=topNum;i++){
     readOut = "";
 
     // For multiples of 3,5,7 read terms as and when and in that order
-    if (i%3 === 0 && rules[0])
-        readOut += "Fizz";
-    if (i%5 === 0 && rules[1])
-        readOut += "Buzz";
-    if (i%7 === 0 && rules[2])
-        readOut += "Bang";
+    if (rules[0])
+        readOut = rule3(i,readOut);
+    if (rules[1])
+        readOut = rule5(i,readOut);
+    if (rules[2])
+        readOut = rule7(i,readOut);
 
-    // For multiples of 11 read "Bong" only (unless also multiple of 13)
-    if (i%11 === 0 && rules[3])
-        readOut = "Bong";
+    // For multiples of 11 read "Bong" only (unless also multiple of 13 in which case read Fezz also)
+    if (rules[3])
+        readOut = rule11(i,readOut);
 
     // For multiples of 13 read Fezz before first B word
-    if (i%13 ===0 && rules[4]) {
-        bIndex = readOut.indexOf("B");
-        if (bIndex !== -1)
-            readOut = readOut.slice(0,bIndex)+ "Fezz" + readOut.slice(bIndex);
-        else
-            readOut += "Fezz";
-    }
+    if (rules[4])
+        readOut = rule13(i,readOut)
 
     // For multiples of 17 reverse order of words
-    if (i%17===0 && rules[5]){
-        minJ = -readOut.length;
-        oldOut = readOut;
-        readOut = "";
-        for (let j=0;j>=minJ;j=j-4){
-            readOut += oldOut.slice(j-4,j);
-        }
-    }
+    if (rules[5])
+        readOut = rule17(i,readOut)
 
     // If not multiple of any:
     if (readOut === "")
